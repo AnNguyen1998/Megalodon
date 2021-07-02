1042e2234c5f16baf7f780a3272a8e5e86ed1945
shivalcrow <58440335+shivalcrow@users.noreply.github.com>
2021-07-02 19:37:56 +0700

    commit

---
+++ src/main/java/com/web/demo/repository/UserRepositorySon.java
@@ -0,0 +1,21 @@
+package com.web.demo.repository;
+/**
+ * @author NguyenHuuSon
+ */
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.web.demo.entity.Users;
+
+@Repository
+public interface UserRepositorySon extends JpaRepository<Users,Integer> {
+
+	Users findByusernameUsers(String username);
+	Optional<Users> findByUsernameUsers(String username);
+	Optional<Users> findByEmailUsers(String emailUsers);
+	
+
+}
