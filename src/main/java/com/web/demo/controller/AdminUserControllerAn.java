284d8021ac58381edc051e1b3afc20fcc8c32273
An Nguyen <conandold@gmail.com>
2021-07-02 14:50:34 +0700

    commit

--- src/main/java/com/web/demo/controller/AdminUserControllerAn.java
+++ src/main/java/com/web/demo/controller/AdminUserControllerAn.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminUserControllerAn {
+	
+	@GetMapping("admin")
+	public String adminindex() {
+		return "admin/index";
+	}
 	@GetMapping("admin/listuser")
 	public String userlist() {
 		System.out.println("Admin/listuser");
