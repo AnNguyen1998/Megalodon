5a5bcc46b6bb5f89b8fb86fa734b9174bf6ed73c
shivalcrow <58440335+shivalcrow@users.noreply.github.com>
2021-07-11 18:11:46 +0700

    add cart

---
+++ src/main/java/com/web/demo/service/BillDetailServiceSon.java
@@ -0,0 +1,15 @@
+package com.web.demo.service;
+
+import com.web.demo.entity.Bill;
+import com.web.demo.entity.BillDetail;
+import com.web.demo.entity.Games;
+
+public interface BillDetailServiceSon {
+
+	<S extends BillDetail> S save(S entity);
+
+	<S extends BillDetail> BillDetail addbilldetail(Bill bill, Games game);
+
+	
+
+}
