6b7ac8e606a75c53ae66486b91dece51b4014de1
AnNguyen <AnNguyen@AnNguyen-PC>
2021-07-13 20:07:57 +0700

    upload multi image

--- src/main/java/com/web/demo/controller/GameControllerAn.java
+++ src/main/java/com/web/demo/controller/GameControllerAn.java
@@ -3,14 +3,14 @@ package com.web.demo.controller;
  * @author An Nguyen
  */
 import java.security.Principal;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,13 +20,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.web.demo.config.WebUtilsAn;
-import com.web.demo.dto.ImageDataMultiAn;
-import com.web.demo.dto.SlideShowDTO;
 import com.web.demo.entity.Category;
 import com.web.demo.entity.Discount;
 import com.web.demo.entity.Games;
 import com.web.demo.entity.ImageData;
-import com.web.demo.entity.SlideShow;
 import com.web.demo.service.AdminBillServiceAn;
 import com.web.demo.service.AdminGameServiceAn;
 import com.web.demo.service.CategoryService;
@@ -52,8 +49,6 @@ public class GameControllerAn {
 	
 	@GetMapping("admin/addgame")
 	public String addgame(Model model, Principal principal) {
-		ImageDataMultiAn img = new ImageDataMultiAn();
-		model.addAttribute("imgdt", img);
 		
 		model.addAttribute("game", new Games());
 		
@@ -91,28 +86,17 @@ public class GameControllerAn {
 	}
 	@RequestMapping(value = "/savegame", method = RequestMethod.POST)
 	public String savegame(@ModelAttribute("game") Games game, @RequestParam("files[]") MultipartFile[] images,
-			@ModelAttribute("imgdt") ImageDataMultiAn imgdt, Model model ) {
+			 Model model ) {
 		Games g = gameService.save(game);
-		int id = g.getIdGame();
-		for(MultipartFile image : images) {
-			try {
-				if(image.getOriginalFilename().isEmpty()) {
-					System.out.println("Empty file name");
-					continue;
-				}
-				imageService.store(image);
-				ImageData img = new ImageData();
-				img.setGames(game);
-				if(!imgdt.getNameImage().isEmpty() && imgdt.getNameImage().getOriginalFilename()!=null) {
-					img.setNameImage(imgdt.getNameImage().getOriginalFilename());
-					model.addAttribute("message", "Add");
-					imageService.save(img);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}			
-		}
-		return "redirect:/admin/Listofgame";
+		Arrays.asList(images).stream().forEach(image -> {
+			imageService.store(image);
+			String file=StringUtils.cleanPath(image.getOriginalFilename());
+			ImageData imagedata=new ImageData();
+			imagedata.setGames(g);
+			imagedata.setNameImage(file);
+			imageService.save(imagedata);
+		});
+		return "redirect:/admin/listgame";
 	}
 	@GetMapping("admin/listgame")
 	public String listgame(Model model, Principal principal) {
