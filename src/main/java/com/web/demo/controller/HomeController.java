f1e51a5e3c509a74024286fad10fc5d17ccc301e
AaronNguyen92 <npdat101092@gmail.com>
2021-07-16 14:58:45 +0700

    add

--- src/main/java/com/web/demo/controller/HomeController.java
+++ src/main/java/com/web/demo/controller/HomeController.java
@@ -138,7 +138,8 @@ public class HomeController {
 		model.addAttribute("games_New", gameService.getGamesByFilter("ReleaseYear_game", 1).get(0));
 		model.addAttribute("images4", imageGameService.getImageDetailGame(gameService.getGamesByFilter("ReleaseYear_game", 1).get(0).getIdGame()).get(0));
 		
-        return "index1";
+        return "index";
+ 
     }
 	
 	/*
