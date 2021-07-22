4e0b58c51e1d1bab1cdaaf8a231e73ee44da6048
shivalcrow <58440335+shivalcrow@users.noreply.github.com>
2021-07-22 13:32:07 +0700

    edit

--- src/main/java/com/web/demo/config/WebSecurityConfig.java
+++ src/main/java/com/web/demo/config/WebSecurityConfig.java
@@ -50,8 +50,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.csrf().disable();
 		http
 				.authorizeRequests()
-				.antMatchers("/admin/","/admin/listusers").access("hasRole('ADMIN')");
-		http.authorizeRequests().antMatchers("/admin/listcustomers","/admin/").access("hasRole('EMPLOYEE')");
+				.antMatchers("/admin/").access("hasRole('ADMIN') or hasRole('EMPLOYEE') ");
+		http.authorizeRequests().antMatchers("/admin/listcustomers").access("hasRole('EMPLOYEE')");
+		http.authorizeRequests().antMatchers("/admin/listusers").access("hasRole('ADMIN')");
 		http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
 		http.authorizeRequests().and()
 				.formLogin().successHandler(customizeAuthenticationSuccessHandler)
